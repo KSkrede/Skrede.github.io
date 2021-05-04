0d0be450aafdc3b4e1abda23d14d6a52e4e40485
Kristian Skrede <44050668+KSkrede@users.noreply.github.com>
2021-05-04 22:26:42 +0200

    t

--- common_variables.R
+++ common_variables.R
@@ -1,5 +1,5 @@
 # Common non-csv data between CV and resume
 
 # Header with description of what is trying to be accomplished with this CV/Resume
-intro_text <- "Hei, eg heter Kristian Skrede og [dette](Kristian.skrede.info), er nettsiden min
+intro_text <- "Hei, eg heter Kristian Skrede og [dette](http://Kristian.skrede.info/), er nettsiden min
 "
