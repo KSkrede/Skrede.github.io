c98d23e31f29b9e2a655b496b2473b6a0b15f3de
Kristian Skrede <44050668+KSkrede@users.noreply.github.com>
2021-05-06 20:34:42 +0200

    Added sertifications and refrences

--- common_variables.R
+++
@@ -1,5 +0,0 @@
-# Common non-csv data between CV and resume
-
-# Header with description of what is trying to be accomplished with this CV/Resume
-intro_text <- "Hei, eg heter Kristian Skrede og [dette](http://Kristian.skrede.info/), er nettsiden min
-"
